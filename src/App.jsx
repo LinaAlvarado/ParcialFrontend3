25bbec2b71a4d95057e80ef427591d5edb0c4a86
LinaAlvarado <linamariaalvaradopallares@gmail.com>
2024-08-12 19:15:21 -0500

    Subiendo archivos

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./Components/Card"
+
+function App() {
+  return (
+    <>
+    <h1>Carga de estudiantes</h1>
+    <form></form>
+     <Card></Card>
+    </>
+  )
+}
+
+export default App
